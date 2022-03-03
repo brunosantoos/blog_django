2719d22773635711ac0638f2e00b87964f555519
Your Name <you@example.com>
2022-03-03 17:10:09 -0300

    Iniciando app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
+from turtle import title
+from venv import create
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255) #criando variavel string / CharField
+    slug = models.SlugField(max_length=255, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #Criando Variavel para o nome do Autor
+    body = models.TextField() #textfield = Maior que string
+    created = models.DateTimeField(auto_now_add=True) # data create
+    update = models.DateTimeField(auto_now=True) #Data update post
+    
