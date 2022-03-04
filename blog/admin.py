c0f664970a1df5756b81b527d3d4e88511f2ef60
Your Name <you@example.com>
2022-03-04 08:27:35 -0300

    Configurando page admin

--- blog/admin.py
+++ blog/admin.py
@@ -7,5 +7,5 @@ from .models import Post
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ("title", "slug", "author", "created", "update")
-    prepopulated_fields = {"slug": ("title",)}
+    prepopulated_fields = {"slug": ("title",)} #preencher automaticamente slug
 # Register your models here.
