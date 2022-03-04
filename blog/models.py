7fb90b10ae4e182cd1c22d49fe70aad5a580c014
Your Name <you@example.com>
2022-03-04 10:49:12 -0300

    Criando viewer post e arquivos base - sem css

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from turtle import title
 from venv import create
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=255) #criando variavel string / CharField
@@ -14,3 +15,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse ("blog:detail", kwargs={"slug": self.slug})
